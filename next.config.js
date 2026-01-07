c5b481b5f5905a1b934d23cbe673a74ebda61ef5
KostyaLKE <kostyalitvinov8@gmail.com>
2026-01-07 10:09:14 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,16 @@
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
